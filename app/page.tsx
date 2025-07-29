942665155b342d72b2e36edd8e274895438cf92e
amorcer-dev <amorcerdev@gmail.com>
2025-07-29 14:47:50 -0400

    Update website

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import { WidgetInsurances } from '../components/WidgetInsurances'
+
+export default function Home() {
+  return (
+    <main className="min-h-screen flex flex-col items-center justify-center p-8 space-y-8">
+      <WidgetInsurances
+
+      />
+    </main>
+  );
+}
